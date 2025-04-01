d6c9506be669696740efac16d2b24338b8e28aff
Xylene <150695680+XyleneChihata@users.noreply.github.com>
2025-04-01 20:15:19 +0800

    Create CutsceneTrigger.cs

---
+++ Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+//package install timeline and cinemachine
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    //public PlayableDirector timeline; // Assign the Timeline asset here
+    //public GameObject player;
+    //public GameObject cutsceneCamera;
+    //public GameObject gameplayCamera;
+
+    void Start()
+    {
+        gameplayCamera.SetActive(true);
+        cutsceneCamera.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            StartCutscene();
+        }
+    }
+
+    void StartCutscene()
+    {
+        gameplayCamera.SetActive(false);
+        cutsceneCamera.SetActive(true);
+        timeline.Play();
+        StartCoroutine(EndCutscene());
+    }
+
+    System.Collections.IEnumerator EndCutscene()
+    {
+        yield return new WaitForSeconds((float)timeline.duration);
+        cutsceneCamera.SetActive(false);
+        gameplayCamera.SetActive(true);
+    }
+}
